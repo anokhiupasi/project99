7f179ddd594c2e4688c007ef5c8eaf3b3b7733cd
anokhiupasi <109988583+anokhiupasi@users.noreply.github.com>
2022-10-12 20:55:21 -0400

    Add files via upload

---
+++ game_login (1).js
@@ -0,0 +1,8 @@
+// Create function addUser()
+function addUser(){
+  window.Location="game_page.html";
+  <input type="player1_name"class="form-control" placeholder="Player 1 Name"></input>
+  <input type="player2_name"class="form-control" placeholder="Player 2 Name"></input>
+}
+
+  
